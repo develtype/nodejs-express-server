40e186e83f60f63dd5be561355fe3eaf383f5c35
develtype <develtype@gmail.com>
2023-02-07 23:51:04 +0900

    refactor: controller with custom decorator

---
+++ src/decorator/controller.decorator.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import { ServerError } from 'src/error/server.error';
+import { ErrorCodeEnum } from 'src/type/error.type';
+
+export function Controller(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor,
+) {
+  const originalMethod = descriptor.value;
+  descriptor.value = async function (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      return await originalMethod.apply(this, [req, res, next]);
+    } catch (err) {
+      if (err instanceof ServerError) {
+        return res.status(err.httpStatus).json({ message: err.message });
+      } else {
+        const error = new ServerError(
+          ErrorCodeEnum.INTERNAL_SERVER_ERROR,
+          'Something unexpected happened in server.',
+        );
+        return res.status(error.httpStatus).json({ message: error.message });
+      }
+    }
+  };
+}
--- src/router/data.router.ts
+++ src/router/data.router.ts
@@ -1,31 +1,16 @@
 import { Router } from 'express';
-import { ServerError } from 'src/error/server.error';
+import { DataController } from 'src/controller/data.controller';
 import { DataRepositoryMemory } from 'src/repository/memory/data.repository.memory';
 import { DataService } from 'src/service/data.service';
-import { ErrorCodeEnum } from 'src/type/error.type';
 
 export function dataRouter() {
   const router = Router();
 
   const dataRepo = new DataRepositoryMemory();
   const dataService = new DataService({ dataRepo });
+  const dataController = new DataController({ dataService });
 
-  router.get('/', async (req, res) => {
-    try {
-      const dataRes = await dataService.getDatas();
-      return res.json(dataRes);
-    } catch (err) {
-      if (err instanceof ServerError) {
-        return res.status(err.httpStatus).json({ message: err.message });
-      } else {
-        const error = new ServerError(
-          ErrorCodeEnum.INTERNAL_SERVER_ERROR,
-          'Something unexpected happened in server.',
-        );
-        return res.status(error.httpStatus).json({ message: error.message });
-      }
-    }
-  });
+  router.get('/', dataController.getDatas);
 
   return router;
 }
